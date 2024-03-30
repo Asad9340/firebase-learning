3499c1b65f5bc7937d44ac4270c51b1431c76988
Asad9340 <asadulimran1999@gmail.com>
2024-03-30 20:24:59 +0600

    changes in file

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import app from './firebase/firebase.config'
 
+const auth = getAuth(app);
+const googleProvider =new GoogleAuthProvider();
 function App() {
-  const [count, setCount] = useState(0)
+  const handleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const logUser = result.user;
+        console.log(logUser);
+    } )
+      .catch((error) => {
+        console.log(error);
+    })
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Firebase + React</h1>
+      <button onClick={handleSignIn}>Google Sign In</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import LogIn from './LogIn/LogIn.1.jsx';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+   <App/>
+  </React.StrictMode>
+);
