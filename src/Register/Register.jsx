e00151ba6a42e06a48af059036b1364972ef12f8
Asad9340 <asadulimran1999@gmail.com>
2024-03-31 02:31:08 +0600

    added eye button on password show and uppercase and number validation on password

--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -1,17 +1,46 @@
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import auth from '../firebase/firebase.config';
+import { useState } from 'react';
+import { IoMdEye } from 'react-icons/io';
+import { IoMdEyeOff } from 'react-icons/io';
+
 function Register() {
-    const handleRegister = event => {
-      event.preventDefault();
-      const name = event.target.fullname.value;
-      const email = event.target.email.value;
-      const password = event.target.password.value;
-      console.log(name);
-      console.log(email);
-      console.log(password);
-    };
+  const [registerError, setRegisterError] = useState('');
+  const [success, setSuccess] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const handleRegister = event => {
+    event.preventDefault();
+    const name = event.target.fullname.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    setRegisterError('');
+    setSuccess('');
+    if (password.length < 6) {
+      setRegisterError('Password must be at least 6 characters');
+      return;
+    } else if (
+      !/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+}{"':;?/.,<>]).{6,}$/.test(
+        password
+      )
+    ) {
+      setRegisterError(
+        'Password must contain Uppercase, Number, and Special characters'
+      );
+      return;
+    }
+    createUserWithEmailAndPassword(auth, email, password, name)
+      .then(userCredential => {
+        console.log(userCredential.user), setSuccess(userCredential.user);
+      })
+      .catch(error => setRegisterError(error.message));
+  };
   return (
     <div>
       <div className="bg-grey-lighter min-h-screen flex flex-col border">
-        <form onSubmit={handleRegister} className="container max-w-md mx-auto flex-1 flex flex-col items-center justify-center px-2">
+        <form
+          onSubmit={handleRegister}
+          className="container max-w-md mx-auto flex-1 flex flex-col items-center justify-center px-2"
+        >
           <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
             <h1 className="mb-8 text-3xl text-center">Sign up</h1>
             <input
@@ -28,18 +57,30 @@ function Register() {
               placeholder="Email"
             />
 
-            <input
-              type="password"
-              className="block border border-grey-light w-full p-3 rounded-lg mb-4"
-              name="password"
-              placeholder="Password"
-            />
-            <input
-              type="password"
-              className="block border border-grey-light w-full p-3 rounded-lg mb-4"
-              name="confirm_password"
-              placeholder="Confirm Password"
-            />
+            <div className="relative">
+              <input
+                type={showPassword ? 'text' : 'password'}
+                className="block border border-grey-light w-full p-3 rounded-lg mb-4"
+                name="password"
+                placeholder="Password"
+              />
+              <span
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute top-4 right-4"
+              >
+                {showPassword ?<IoMdEye />:<IoMdEyeOff />}
+              </span>
+            </div>
+            {registerError && (
+              <p className="text-lg bg-red-500 px-8 py-2 rounded-lg text-white flex justify-center mx-auto text-center my-4">
+                {registerError}
+              </p>
+            )}
+            {success && (
+              <p className="text-lg bg-green-500 px-8 py-2 rounded-lg text-white flex justify-center mx-auto text-center my-4">
+                Successfully Created
+              </p>
+            )}
 
             <button
               type="submit"
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -7,6 +8,7 @@ import { initializeApp } from 'firebase/app';
 const firebaseConfig = {
   apiKey: 'AIzaSyBg1mVbsbB29WTD349-8XFOHImlfOLwO4M',
   authDomain: 'fir-learning-305fb.firebaseapp.com',
+  databaseURL: 'https://fir-learning-305fb-default-rtdb.firebaseio.com',
   projectId: 'fir-learning-305fb',
   storageBucket: 'fir-learning-305fb.appspot.com',
   messagingSenderId: '609507411237',
@@ -15,4 +17,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export default app;
+const auth = getAuth(app);
+export default auth;
